c427d33071dde60e91db48fa0d24cbdb6582e35a
muradtkk <muradtkk@RUBIS05>
2025-11-07 21:42:39 +0400

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,156 @@
+﻿#region Tapsiriq 1
+//int a;
+//for (int saygac = 0; saygac < 10; saygac++)
+//{
+//    Console.WriteLine(saygac);
+//    a = int.Parse(Console.ReadLine());
+//}
+//Console.WriteLine("Ededi Daxil Edin");
+//int eded = int.Parse(Console.ReadLine()!);
+//if (eded   % 2 == 0)
+//{
+//    Console.WriteLine("Cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("Tekdir");
+//}
+#endregion
+#region Tapsiriq 2
+//Console.WriteLine("Ededi Daxil Edin");
+//int eded1 = int.Parse(Console.ReadLine()!);
+
+
+//Console.WriteLine("2 ci Ededi Daxil Edin");
+//int eded2 = int.Parse(Console.ReadLine()!);
+//if (eded1 > eded2)
+//{
+//    Console.WriteLine("eded1 Boyukdur");
+//}
+//else
+//{
+//    Console.WriteLine("eded2 Boyukdur");
+//}
+using System.ComponentModel.Design;
+#endregion
+#region Tapsiriq 3
+//Console.WriteLine("Ededi Daxil Edin");
+//int eded1 = int.Parse(Console.ReadLine()!);
+
+//if (eded1 > 0)
+//{
+//    Console.WriteLine("Eded Musbetdir");
+//}
+//else if (eded1 < 0)
+//{
+//    Console.WriteLine("Eded Musbetdir");
+//}
+//else
+//{
+//    Console.WriteLine("Sifirdir");
+//}
+#endregion
+#region Tapsiriq 4
+//Console.WriteLine("n ededini daxil edin:");
+//int n = int.Parse(Console.ReadLine()!);
+
+//for (int i = 1; i <= n; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Console.WriteLine("n ededini daxil edin:");
+//int n = int.Parse(Console.ReadLine()!);
+
+//int sum = 0;
+
+//for (int i = 1; i <= n; i++)
+//{
+//    sum += i;
+//}
+#endregion
+#region Tapsiriq 5
+//Console.WriteLine($"Cem = {sum}");
+
+//Console.Write("Ededi daxil edin: ");
+//int n = int.Parse(Console.ReadLine()!);
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine($"{n} x {i} = {n * i}");
+//}
+
+//
+//Console.Write("Ededi daxil edin: ");
+//int n = int.Parse(Console.ReadLine()!);
+
+//int say = 0;
+//int temp = Math.Abs(n);
+
+//for (; temp > 0; temp /= 10)
+//{
+//    say++;
+//}
+
+//Console.WriteLine($"Eded {say} reqemlidir");
+#endregion
+#region  Tapsiriq 6
+//Console.WriteLine("ededi daxil edin ");
+//int n = int.Parse(Console.ReadLine()!);
+//int reverse = 0;
+
+//while (n > 0)
+//{
+//    int qaliq = n % 10;
+//    reverse = reverse * 10 + qaliq;
+//    n = n / 10;
+//}
+
+//Console.WriteLine(reverse);
+#endregion
+#region Tapsiriq 7
+//Console.Write("n ededini daxil edin: ");
+//int n = int.Parse(Console.ReadLine()!);
+
+//int result = 1;
+
+//for (int i = 1; i <= n; i++)
+//{
+//    result = result * i;
+//}
+
+//Console.WriteLine($"{n} = {result}");
+#endregion
+#region Tapsiriq 8
+
+#endregion
+#region Tapsiriq 9
+
+#endregion
+#region Tapsiriq 10
+
+#endregion
+#region Tapsiriq 11
+
+#endregion
+#region Tapsiriq 12
+
+#endregion
+#region Tapsiriq 13
+
+#endregion
+#region Tapsiriq 14
+
+#endregion
+#region Tapsiriq 15
+
+#endregion
+#region Tapsiriq 16
+
+#endregion
+#region Tapsiriq 17 
+
+#endregion
+#region Tapsiriq 18
+
+#endregion
